563e10c8eb5ead15ecd33947b8f87c36612f29e1
Esteban <esteband@udel.edu>
2018-04-05 10:38:55 -0400

    message

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,8 @@
+blist = ["end of semester", "blah blah", "blah blah blah"];
+console.log(blist.length);
+blist.push("more blahs");
+console.log(blist);
+
+for (i = 0; i < blist.length; i++) {
+  console.log(blist[i]);
+}
