746236bce3fd4774bb6e30f53bb2ead432f61b3d
Esteban <esteband@udel.edu>
2018-04-19 10:23:39 -0400

    add exercises

---
+++ 4-18-18/python-basics.py
@@ -0,0 +1,7 @@
+#   this is my first python code
+greeting = "hello"
+name ="esteban"
+print greeting
+print str(len(greeting)) + " " + greeting.upper()
+print greeting[4]
+print "%s %d!" %(greeting, len(name)) # string formating
